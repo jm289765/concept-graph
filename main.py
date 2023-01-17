8b98ed4549a73f6dfc0a6bd635fe0a9ae03e02d2
jm289765 <112527928+jm289765@users.noreply.github.com>
2023-01-17 21:05:15 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def run():
+    try:
+        while True:
+            inp = input("> ")
+            print(inp)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    run()
